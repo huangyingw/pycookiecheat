83a6aafff51770785736b464272d8a985715c9ae
Ying Huang <huangyingw@gmail.com>
2020-06-30 20:41:36 -0700

    n

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
-
 from pycookiecheat import chrome_cookies
 import requests
 
-url = 'http://example.com/fake.html'
+url = 'https://api.imvu.com/login/me/'
 
 # Uses Chrome's default cookies filepath by default
-cookies = chrome_cookies(url)
+cookies = chrome_cookies(
+    url, cookie_file='/Users/huangyingw/Library/Application Support/Google/Chrome/Default/Cookies')
 r = requests.get(url, cookies=cookies)
+print('r --> %s' % r)
